84172ce590df7df19098ddc063fb36abd42b0ad7
silvinavd <silvinavalverde8@gmail.com>
2021-04-22 13:36:41 +0000

    randomOK

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var suit = ["♦", "♥", " ♠", "♣"];
+  var number = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomsuit = Math.floor(Math.random() * suit.length);
+  let randomnumber = Math.floor(Math.random() * number.length);
+
+  console.log(suit[randomsuit] + number[randomnumber]);
 };
